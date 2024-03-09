21ed62f3a61b491bede862e87081f4104e4b2ea7
Ubuntu <ubuntu@ip-172-31-59-40.ec2.internal>
2024-03-09 05:20:42 +0000

    made changes

---
+++ jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# install java on ubuntu
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+java -version
+openjdk version "17.0.8" 2023-07-18
+OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
+OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+
+# Install Jenkins on ubuntu
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
